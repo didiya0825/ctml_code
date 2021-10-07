c0254c5181cdce035483cec2d69ddac6ce4a9ed6
didiya0825 <91994268+didiya0825@users.noreply.github.com>
2021-10-08 00:37:14 +0800

    Update utils.py

--- few_shot_image/modules/utils.py
+++ few_shot_image/modules/utils.py
@@ -34,3 +34,36 @@ def conv_block(inp, cweight, bweight, reuse, scope, activation=tf.nn.relu, max_p
 
 def xent(pred, label, size):
     return tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=label) / size
+
+
+def student_t_similarity(inputs, centroids, alpha=1.):
+    """
+    :param inputs: [1, embed_dim]
+    :param centroids: [num_cluster, embed_dim]
+    :param alpha: degrees of freedom, scalar
+    :return: soft assignment [1, num_cluster]
+    """
+    distance = tf.reduce_sum(tf.square(inputs - centroids), axis=-1)  # [num_cluster]
+    distance = tf.pow(1 + distance / alpha, - (alpha + 1.) / 2.)  # [num_cluster]
+    soft_assignment = tf.expand_dims(distance / tf.reduce_sum(distance), axis=0)  # [1, num_cluster]
+    return soft_assignment
+
+
+def kl_divergence(p, q):
+    p = tf.reshape(p, [-1])
+    q = tf.reshape(q, [-1])
+    return tf.reduce_sum(p * tf.log(p / q) / tf.log(2.))
+
+
+def js_divergence(p, q):
+    p = tf.reshape(p, [-1])
+    q = tf.reshape(q, [-1])
+    m = 0.5 * (p + q)
+    return 0.5 * kl_divergence(p, m) + 0.5 * kl_divergence(q, m)
+
+
+def mse(pred, label):
+    pred = tf.reshape(pred, [-1])
+    label = tf.reshape(label, [-1])
+    return tf.reduce_mean(tf.square(pred - label))
+
